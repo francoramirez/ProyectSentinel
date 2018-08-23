c74a766d6cdfda2e186e4045f4fa2d95fa531e3c
francoramirez <francoramirezm@gmail.com>
2018-08-23 18:29:35 -0500

    Avance del login dia 4 e inicio del consumo de web service

--- sentinelProyect/src/app/app.component.ts
+++ sentinelProyect/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sentinelProyect';
+  user: string
+  password: string
+  cookieValid: boolean
+  plataform: string
 }
